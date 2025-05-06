1201715ef1491cb2bdc9dcf36fdb6e65c81ae156
Roozbeh Zarei <roozbehzarei@proton.me>
2025-05-06 22:30:44 +0330

    Setup Hilt DI

---
+++ app/src/main/java/com/roozbehzarei/meowpedia/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.roozbehzarei.meowpedia
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application()
--- app/src/main/java/com/roozbehzarei/meowpedia/MainActivity.kt
+++ app/src/main/java/com/roozbehzarei/meowpedia/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.roozbehzarei.meowpedia.ui.theme.MeowpediaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
